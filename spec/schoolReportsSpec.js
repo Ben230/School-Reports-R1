5844c7f736e78060962857a3b4f4ba2c2af92596
Ben Stuart <benarthurstuart@hotmail.com>
2019-12-20 16:34:36 +0000

    beforeEach loop in tests

--- spec/schoolReportsSpec.js
+++ spec/schoolReportsSpec.js
@@ -2,18 +2,21 @@ var reportModule = require('../src/schoolReports.js');
 var Report = reportModule.Report;
 
 describe('school report', function() {
+  var schoolReport;
+
+  beforeEach(function(){
+    schoolReport = new Report;
+  });
+
   it('returns empty string for no input', function() {
-    var schoolReport = new Report;
     expect(schoolReport.calculate('')).toEqual('');
   });
 
   it('returns "green: 1" for input of "green"', function() {
-    var schoolReport = new Report;
     expect(schoolReport.calculate('green')).toEqual('green: 1');
   });
 
   it('returns "green: 2" for input of "green, green"', function() {
-    var schoolReport = new Report;
     expect(schoolReport.calculate('green, green')).toEqual('green: 2');
   });
 });
